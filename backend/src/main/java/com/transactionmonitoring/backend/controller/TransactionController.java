57f0da661a1875e160120fc030eed0a19af8d081
DevAniket010 <fs20co010@gmail.com>
2026-08-05 11:19:47 +0000

    Integrate front-end and back-end

--- backend/src/main/java/com/transactionmonitoring/backend/controller/TransactionController.java
+++ backend/src/main/java/com/transactionmonitoring/backend/controller/TransactionController.java
@@ -6,16 +6,25 @@ import org.springframework.web.bind.annotation.RestController;
 import com.transactionmonitoring.backend.service.TransactionService;
 import org.springframework.web.bind.annotation.RequestBody;
 import com.transactionmonitoring.backend.entity.Transaction;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 
 
 @RestController
-@RequestMapping("/transactions")
+@RequestMapping("/api/transactions")
 public class TransactionController {
     private final TransactionService transactionService;
     public TransactionController(TransactionService transactionService){
         this.transactionService = transactionService;
     }
+
+    @GetMapping
+    public List<Transaction> getAllTransactions() {
+        return transactionService.getAllTransactions();
+    }
+
     @PostMapping
     public Transaction saveTransaction(@RequestBody Transaction transaction){
         return transactionService.saveTransaction(transaction);
